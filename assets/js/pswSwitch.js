19c59d8bf24f1d87859c123555f9851bb91e2ed8
ccorreas Mint <ciclost@ip-192-168-2-32>
2023-10-20 19:11:15 +0200

    Cuenta[Login OK]

--- assets/js/pswSwitch.js
+++ assets/js/pswSwitch.js
@@ -1,16 +1,15 @@
 const pswOff = document.querySelector("#psw-off");
 const pswOn = document.querySelector("#psw-on");
-const pswInput = document.querySelector(".psw__input");
+const pswInput = document.querySelector("#account-login-psw");
 
-pswOff.addEventListener("click", (e)=>{
-    pswInput.type = "text";
-    pswOff.classList.remove("login__icon--active")
-    pswOn.classList.add("login__icon--active")
-})
+pswOff.addEventListener("click", (e) => {
+  pswInput.type = "text";
+  pswOff.classList.remove("account__icon--active");
+  pswOn.classList.add("account__icon--active");
+});
 
-
-pswOn.addEventListener("click", (e)=>{
-    pswInput.type = "password";
-    pswOn.classList.remove("login__icon--active")
-    pswOff.classList.add("login__icon--active")
-})
+pswOn.addEventListener("click", (e) => {
+  pswInput.type = "password";
+  pswOn.classList.remove("account__icon--active");
+  pswOff.classList.add("account__icon--active");
+});
