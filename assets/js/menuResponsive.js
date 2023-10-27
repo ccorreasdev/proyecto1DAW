0d03b9e9dd260731e64a92f7fe473ac90ff07fb4
ccorreas Mint <ciclost@ip-192-168-2-32>
2023-10-27 19:50:46 +0200

    Menu responsive funcionando, pendiente de mejoras

--- assets/js/menuResponsive.js
+++ assets/js/menuResponsive.js
@@ -1,7 +1,18 @@
 const responsiveBtn = document.querySelector("#nav-btn");
 const layoutNavMenu = document.querySelector(".layout__nav");
+const layoutNavResponsiveMenu = document.querySelector(
+  ".layout__nav-responsive"
+);
+
+let isResponsiveMenuActive = false;
 
 responsiveBtn.addEventListener("click", (e) => {
-  console.log("MENUUUU");
-  layoutNavMenu.classList.toggle("responsive__nav--show");
+  layoutNavMenu.classList.remove("layout__nav--hidden");
+  if (isResponsiveMenuActive) {
+    layoutNavResponsiveMenu.style.opacity = 0;
+    isResponsiveMenuActive = false;
+  } else {
+    layoutNavResponsiveMenu.style.opacity = 1;
+    isResponsiveMenuActive = true;
+  }
 });
