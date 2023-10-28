a232c6e63398ec20e9e47654aad7704c149e9730
ccorreas <correasberenguer@gmail.com>
2023-10-28 17:18:34 +0200

    [Información - Responsive]

--- assets/js/menuResponsive.js
+++ assets/js/menuResponsive.js
@@ -1,34 +1,72 @@
-const responsiveBtn = document.querySelector("#nav-btn");
-const layoutNavMenu = document.querySelector(".layout__nav");
-const layoutNavResponsiveMenu = document.querySelector(
+const layoutNav = document.querySelector(".layout__nav");
+const bgCanvas = document.querySelector(".canvas__background");
+const lnCanvas = document.querySelector(".canvas__lines");
+const mediaResponsiveBtn = document.querySelector("#nav-btn");
+const mediaLayoutNavMenu = document.querySelector(".layout__nav");
+const mediaLayoutNavResponsiveMenu = document.querySelector(
   ".layout__nav-responsive"
 );
+let isMediaStarted = true;
+let screenWidth = window.matchMedia("(max-width: 1280px)");
+let isMediaResponsiveMenuActive = false;
+layoutNav.style.transitionDuration = "300ms";
 
-let isResponsiveMenuActive = false;
-
-responsiveBtn.addEventListener("click", (e) => {
-  layoutNavMenu.classList.remove("layout__nav--hidden");
-  if (isResponsiveMenuActive) {
-    layoutNavResponsiveMenu.style.opacity = 0;
-    isResponsiveMenuActive = false;
-    layoutNavMenu.style.pointerEvents = "none";
+mediaResponsiveBtn.addEventListener("click", (e) => {
+  layoutNav.classList.remove("layout__nav--hidden");
+  if (isMediaResponsiveMenuActive) {
+    mediaLayoutNavResponsiveMenu.style.opacity = 0;
+    isMediaResponsiveMenuActive = false;
+    layoutNav.style.pointerEvents = "none";
+    mediaResponsiveBtn.classList.remove("nav__responsive-btn--active");
   } else {
-    layoutNavResponsiveMenu.style.opacity = 1;
-    isResponsiveMenuActive = true;
-    layoutNavMenu.style.pointerEvents = "all";
+    mediaLayoutNavResponsiveMenu.style.opacity = 1;
+    isMediaResponsiveMenuActive = true;
+    layoutNav.style.pointerEvents = "all";
+    mediaResponsiveBtn.classList.add("nav__responsive-btn--active");
   }
 });
 
 window.addEventListener("resize", (e) => {
-  let screenWidth = window.matchMedia("(min-width: 1280px)");
+  screenWidth = window.matchMedia("(max-width: 1280px)");
 
-  if (screenWidth.matches) {
-    layoutNavResponsiveMenu.style.opacity = 1;
-    isResponsiveMenuActive = true;
-    layoutNavMenu.style.pointerEvents = "all";
-  } else {
-    layoutNavResponsiveMenu.style.opacity = 0;
-    isResponsiveMenuActive = false;
-    layoutNavMenu.style.pointerEvents = "none";
+  if (isMediaStarted) {
+    if (screenWidth.matches) {
+      mediaResponsiveBtn.classList.remove("nav__responsive-btn--active");
+      mediaLayoutNavResponsiveMenu.style.opacity = 0;
+      isMediaResponsiveMenuActive = false;
+      layoutNav.style.pointerEvents = "none";
+      mediaLayoutNavMenu.setAttribute("pointer-events", "all");
+      layoutNav.classList.add("layout__nav--hidden");
+      mediaResponsiveBtn.setAttribute("pointer-events", "all");
+      mediaResponsiveBtn.classList.remove("nav__responsive-btn--hidden");
+    } else {
+      mediaResponsiveBtn.classList.remove("nav__responsive-btn--active");
+      mediaLayoutNavResponsiveMenu.style.opacity = 1;
+      isMediaResponsiveMenuActive = false;
+      layoutNav.style.pointerEvents = "all";
+      mediaLayoutNavMenu.setAttribute("pointer-events", "all");
+      layoutNav.classList.remove("layout__nav--hidden");
+      mediaResponsiveBtn.setAttribute("pointer-events", "none");
+      mediaResponsiveBtn.classList.add("nav__responsive-btn--hidden");
+    }
   }
 });
+
+screenWidth = window.matchMedia("(max-width: 1280px)");
+
+if (screenWidth.matches) {
+  mediaLayoutNavResponsiveMenu.style.opacity = 0;
+  isMediaResponsiveMenuActive = false;
+  layoutNav.style.pointerEvents = "none";
+  mediaLayoutNavMenu.setAttribute("pointer-events", "all");
+  layoutNav.classList.add("layout__nav--hidden");
+  mediaResponsiveBtn.setAttribute("pointer-events", "all");
+  mediaResponsiveBtn.classList.remove("nav__responsive-btn--hidden");
+} else {
+  mediaResponsiveBtn.setAttribute("pointer-events", "none");
+  mediaResponsiveBtn.classList.add("nav__responsive-btn--hidden");
+}
+
+layoutNav.classList.remove("layout__nav--hidden");
+bgCanvas.classList.remove("canvas__background--hidden");
+lnCanvas.classList.remove("canvas__lines--hidden");
